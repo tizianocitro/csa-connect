7a58287e30083b6346d489cd2ba3a159d6df45c6
Tiziano <tizianocitro98@gmail.com>
2023-02-09 12:18:57 +0000

    Event for rhs resize

--- webapp/src/components/icons/icons.tsx
+++ webapp/src/components/icons/icons.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {useSelector} from 'react-redux';
 
-export const ChannelHeaderButtonIcon = () => (
-    <i
-        className='icon fa fa-plug'
-        style={{fontSize: '15px', position: 'relative', top: '-1px'}}
-    />
-);
+// import qs from 'qs';
+// import {useLocation} from 'react-router-dom';
+import {getCurrentChannelId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/common';
+
+export const ChannelHeaderButtonIcon = () => {
+    const channelID = useSelector(getCurrentChannelId);
+    const icon = useRef<HTMLElement>(null);
+
+    // const location = useLocation();
+    // const queryParams = qs.parse(location.search, {ignoreQueryPrefix: true});
+
+    // const parent = document.getElementById('open-product-rhs')?.parentElement;
+    useEffect(() => {
+        const parent = icon.current?.parentElement;
+        parent?.click();
+        return () => {
+            parent?.click();
+        };
+    }, [channelID]);
+
+    /* const updateStorage = () => {
+        localStorage.setItem('refresh', 'true');
+    };
+    useEffect(() => {
+        window.addEventListener('beforeunload', updateStorage);
+        return () => {
+            window.removeEventListener('beforeunload', updateStorage);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (localStorage.getItem('refresh') === 'true') {
+            const parent = icon.current?.parentElement;
+            parent?.click();
+            localStorage.setItem('refresh', 'false');
+        }
+    }, []); */
+
+    return (
+        <i
+            className='icon fa fa-plug'
+            style={{fontSize: '15px', position: 'relative', top: '-1px'}}
+            id={'open-product-rhs'}
+            ref={icon}
+        />
+    );
+};
 
 export const MainMenuMobileIcon = () => (
     <i className='icon fa fa-plug'/>
--- webapp/src/components/rhs/right_hand_sidebar.tsx
+++ webapp/src/components/rhs/right_hand_sidebar.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-import {FormattedMessage} from 'react-intl';
 
+// import {FormattedMessage} from 'react-intl';
 import {getCurrentChannelId} from 'mattermost-webapp/packages/mattermost-redux/src/selectors/entities/common';
 import {getCurrentTeamId, getTeam} from 'mattermost-redux/selectors/entities/teams';
 import {getChannel} from 'mattermost-redux/selectors/entities/channels';
@@ -22,6 +22,28 @@ const teamNameSelector = (teamId: string) => (state: GlobalState): Team => getTe
 const channelNameSelector = (channelId: string) => (state: GlobalState): Channel => getChannel(state, channelId);
 
 const RHSView = () => {
+    const [status, setStatus] = useState('Closed');
+    const updateStatus = () => {
+        if (status === 'Closed') {
+            setStatus('Open');
+        } else {
+            setStatus('Closed');
+        }
+    };
+    const resetStatus = () => {
+        setStatus('Closed');
+    };
+
+    // const [closed, setClosed] = useState(false);
+    // const updateClosed = () => {
+    //     setClosed(true);
+    // };
+    // const resetClosed = () => {
+    //     setClosed(false);
+    // };
+
+    // TODO: Make the productId dynamic using an API that gives back the id given the channel name
+    // If the channel is not associated to a product, show a message that tells so
     const maybeProduct = useProduct('1');
     const product = maybeProduct as Product;
     const {hash: urlHash} = useLocation();
@@ -30,12 +52,40 @@ const RHSView = () => {
     const teamId = useSelector(getCurrentTeamId);
     const team = useSelector(teamNameSelector(teamId));
     const channel = useSelector(channelNameSelector(channelID));
-
     const fullUrl = `${team.name}/channels/${channel.name}`;
+
+    useEffect(() => {
+        resetStatus();
+    }, [channelID]);
+
+    useEffect(() => {
+        (document.getElementsByClassName('sidebar--right__expand btn-icon')[0] as HTMLElement).addEventListener('click', updateStatus);
+
+        return () => {
+            (document.getElementsByClassName('sidebar--right__expand btn-icon')[0] as HTMLElement).removeEventListener('click', updateStatus);
+        };
+    }, [status]);
+
+    // useEffect(() => {
+    //    if (closed) {
+    //    const parent = document.getElementById('open-product-rhs')?.parentElement;
+    //    parent?.click();
+    //        resetClosed();
+    //    }
+    // }, [channelID]);
+
+    // document.getElementsByClassName('icon icon-close')[0].click();
+    // useEffect(() => {
+    //    (document.getElementsByClassName('icon icon-close')[0] as HTMLElement)?.addEventListener('click', updateClosed);
+    //    return () => {
+    //        (document.getElementsByClassName('icon icon-close')[0] as HTMLElement)?.removeEventListener('click', updateStatus);
+    //    };
+    // }, [closed]);
+
     return (
         <div style={style.rhs}>
             <h1>
-                <FormattedMessage defaultMessage='Whatever'/>
+                {`${status} - ${channel.display_name}`}
             </h1>
             <br/>
             <Table
