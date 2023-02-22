4e24a574b6f793ce6d0d5b5495137733761a7d79
Tiziano <tizianocitro98@gmail.com>
2023-02-22 17:23:44 +0000

    Minor fix to channels widget

--- webapp/src/components/backstage/widgets/channels/channels_list/row.tsx
+++ webapp/src/components/backstage/widgets/channels/channels_list/row.tsx
@@ -1,22 +1,16 @@
+import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
 import React from 'react';
 import styled from 'styled-components';
 import {useSelector} from 'react-redux';
-import {getCurrentTeamId, getTeam} from 'mattermost-redux/selectors/entities/teams';
-import {getChannel} from 'mattermost-redux/selectors/entities/channels';
-import {GlobalState} from 'mattermost-webapp/packages/types/src/store';
-import {Team} from 'mattermost-webapp/packages/types/src/teams';
-import {Channel} from 'mattermost-webapp/packages/types/src/channels';
 
-import {navigateToUrl} from 'src/browser_routing';
-import {WidgetChannel} from 'src/types/channels';
 import {PARENT_ID_PARAM, SECTION_ID_PARAM} from 'src/constants';
+import {channelNameSelector, teamNameSelector} from 'src/selectors';
+import {WidgetChannel} from 'src/types/channels';
+import {navigateToUrl} from 'src/browser_routing';
 
 interface Props {
     channel: WidgetChannel;
 }
-
-const teamNameSelector = (teamId: string) => (state: GlobalState): Team => getTeam(state, teamId);
-const channelNameSelector = (channelId: string) => (state: GlobalState): Channel => getChannel(state, channelId);
 
 const Row = (props: Props) => {
     const channel = useSelector(channelNameSelector(props.channel.channelId));
@@ -29,6 +23,7 @@ const Row = (props: Props) => {
     const openChannel = (channelToOpenId: string, channelToOpenParentId: string, channelToOpenSectionId: string) => {
         navigateToUrl(`/${team.name}/channels/${channelToOpenId}?${SECTION_ID_PARAM}=${channelToOpenSectionId}&${PARENT_ID_PARAM}=${channelToOpenParentId}&from=channel_list`);
     };
+
     return (
         <>
             {channel &&
