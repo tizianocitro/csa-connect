73c086f6ed037c614e3e77f8a6dba93f33c9a0ee
Tiziano <tizianocitro98@gmail.com>
2023-03-09 17:06:43 +0000

    Fixed hyperlinking in accordion widget

--- webapp/src/components/backstage/widgets/accordion/accordion.tsx
+++ webapp/src/components/backstage/widgets/accordion/accordion.tsx
@@ -1,6 +1,7 @@
-import React, {ElementType, useContext} from 'react';
+import React, {ElementType, useContext, useEffect} from 'react';
 import {Collapse} from 'antd';
 import styled from 'styled-components';
+import {useLocation} from 'react-router-dom';
 
 import {AccordionData} from 'src/types/accordion';
 import {AnchorLinkTitle, Header} from 'src/components/backstage/widgets/shared';
@@ -32,8 +33,44 @@ const Accordion = ({
     childComponent: ChildComponent,
     ...props
 }: Props) => {
+    const {hash: urlHash} = useLocation();
     const isEcosystemRhs = useContext(IsEcosystemRhsContext);
     const fullUrl = useContext(FullUrlContext);
+
+    useEffect(() => {
+        if (urlHash) {
+            const panels = document.getElementsByClassName('ant-collapse-item') as HTMLCollectionOf<HTMLElement>;
+            if (!panels) {
+                return;
+            }
+            for (let i = 0; i < panels.length; i++) {
+                const panel = panels[i];
+                const referencedElements = panel?.querySelectorAll(`#${urlHash.substring(1)}`);
+                if (referencedElements && referencedElements.length > 0) {
+                    const header = panel.querySelector('.ant-collapse-header') as HTMLElement;
+                    if (header && !panel.classList.contains('ant-collapse-item-active')) {
+                        header.click();
+                    }
+                }
+
+                // const content = panel.querySelector('.ant-collapse-content') as HTMLElement;
+                // const header = panel.querySelector('.ant-collapse-header') as HTMLElement;
+                // if (referencedElements) {
+                //     content.classList.remove('ant-collapse-content-inactive');
+                //     content.classList.remove('ant-collapse-content-hidden');
+                //     content.classList.add('ant-collapse-content-active');
+                //     panel.classList.add('ant-collapse-item-active');
+                //     header.ariaExpanded = 'true'
+                // } else {
+                //     panel.classList.remove('ant-collapse-item-active');
+                //     content.classList.remove('ant-collapse-content-active');
+                //     content.classList.add('ant-collapse-content-inactive');
+                //     content.classList.add('ant-collapse-content-hidden');
+                //     header.ariaExpanded = 'true'
+                // }
+            }
+        }
+    }, [urlHash]);
 
     const id = `${formatName(name)}-${sectionId}-${parentId}-widget`;
 
@@ -53,19 +90,23 @@ const Accordion = ({
             </Header>
             {elements && elements.length > 0 &&
                 <Collapse
-                    defaultActiveKey={[elements[0].id]}
                     accordion={true}
+                    defaultActiveKey={`${elements[0].id}-panel-key`}
                 >
                     {elements.map((element) => (
-                        <Panel
-                            key={element.id}
-                            header={element.name}
-                        >
-                            <ChildComponent
-                                element={element}
-                                {...props}
-                            />
-                        </Panel>
+                        <>
+                            <Panel
+                                key={`${element.id}-panel-key`}
+                                header={element.name}
+                                id={`${element.id}-panel-key`}
+                                forceRender={true}
+                            >
+                                <ChildComponent
+                                    element={element}
+                                    {...props}
+                                />
+                            </Panel>
+                        </>
                     ))}
                 </Collapse>}
         </Container>
