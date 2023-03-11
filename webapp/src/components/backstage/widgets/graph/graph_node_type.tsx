9445e887edcd6115c397031a5ee182c95fc3df19
Tiziano <tizianocitro98@gmail.com>
2023-03-11 17:17:34 +0000

    Added shapes for different types of nodes

--- webapp/src/components/backstage/widgets/graph/graph_node_type.tsx
+++ webapp/src/components/backstage/widgets/graph/graph_node_type.tsx
@@ -12,6 +12,7 @@ import {PARENT_ID_PARAM, SECTION_ID_PARAM} from 'src/constants';
 import {CopyLinkMenuItem} from 'src/components/backstage/header/controls';
 import {getSiteUrl} from 'src/clients';
 import {GraphSectionOptions} from 'src/types/graph';
+import {buildMap} from 'src/hooks';
 
 export const edgeType = 'step';
 export const nodeType = 'graphNodeType';
@@ -66,6 +67,12 @@ export const fillNodes = (
     return filledNodes;
 };
 
+const nodeKindMap = buildMap([
+    {key: 'switch', value: '5px'},
+    {key: 'server', value: '10px'},
+    {key: 'vpn-server', value: '0px'},
+]);
+
 // These can be alternatives to nodes color
 // background: 'rgb(var(--button-bg-rgb), 0.4)',
 // border: '1px solid rgb(var(--button-bg-rgb), 0.2)',
@@ -74,11 +81,12 @@ const GraphNodeType = ({id, data}: NodeProps) => {
         <>
             <Handle
                 type={'target'}
-                position={Position.Top}
+                position={Position.Left}
             />
             <NodeContainer
                 id={`${id}-${data.sectionId}-${data.parentId}`}
                 isUrlHashed={data.isUrlHashed}
+                kind={data.kind}
             >
                 <CopyLinkMenuItem
                     path={`${data.url}#${id}-${data.sectionId}-${data.parentId}`}
@@ -89,16 +97,16 @@ const GraphNodeType = ({id, data}: NodeProps) => {
             </NodeContainer>
             <Handle
                 type={'source'}
-                position={Position.Bottom}
+                position={Position.Right}
             />
         </>
     );
 };
 
-const NodeContainer = styled.div<{isUrlHashed: boolean}>`
+const NodeContainer = styled.div<{isUrlHashed: boolean, kind: string}>`
     background: ${(props) => (props.isUrlHashed ? 'rgba(var(--center-channel-color-rgb), 0.08)' : 'var(--center-channel-bg)')};
     border: 1px solid rgba(var(--center-channel-color-rgb), 0.8);
-    border-radius: 5;
+    border-radius: ${(props) => nodeKindMap.get(props.kind)};;
 `;
 
 export default GraphNodeType;
